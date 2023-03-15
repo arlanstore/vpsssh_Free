4bee78c209a8118657016870afb7ca6051642bf1
arlanstore <122625984+arlanstore@users.noreply.github.com>
2023-03-15 21:16:19 +0700

    Update portsshnontls.sh

--- websocket/portsshnontls.sh
+++ websocket/portsshnontls.sh
@@ -13,13 +13,13 @@ LIGHT='\033[0;37m'
 # Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/owl64/vpsssh_Free/main/ipvps.txt | grep $MYIP );
+IZIN=$( curl https://raw.githubusercontent.com/arlanstore/vpsssh_Free/main/ipvps.txt | grep $MYIP );
 if [ $MYIP = $IZIN ]; then
 echo -e "${NC}${GREEN}Permission Accepted...${NC}"
 else
 echo -e "${NC}${RED}Permission Denied!${NC}";
 echo -e "${NC}${LIGHT}Please Contact Admin!!"
-echo -e "${NC}${LIGHT}Telegram : https://t.me/owl64"
+echo -e "${NC}${LIGHT}Telegram : https://t.me/arlanstore"
 exit 0
 fi
 clear
